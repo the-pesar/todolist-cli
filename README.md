1420558d3f03d02a6179e11a1681ba00e81aa8eb
Pesar <pesar369@gmail.com>
2024-04-22 17:42:29 +0330

    feat: add list subcommand

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Golang
 ## tasks
 - add: todolist add -n {name} -d {description}
-- read: todolist get {id}
+- read: todolist list {id}
 - update: todolist update {id} -n {newname} -d {newdesc}
 - delete: todolist remove {id}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 var path string = "./todos.json"
@@ -16,31 +17,55 @@ type Todo struct {
 	Status string
 }
 
+var todos = []Todo{}
+
 func main() {
 	add := flag.NewFlagSet("add", flag.ExitOnError)
 	name := add.String("n", "", "Name of the todo item (shorthand)")
 	desc := add.String("d", "", "Description of the todo item (shorthand)")
+
+	get := flag.NewFlagSet("get", flag.ExitOnError)
 	flag.Parse()
 
 	var command string = flag.Arg(0)
 
+	content, _ := os.ReadFile(path)
+	json.Unmarshal(content, &todos)
+
 	switch command {
-		
+
+	// add subcommand function
 	case "add":
 		add.Parse(flag.Args()[1:])
 		fmt.Println(*name, *desc)
+
 		todo := Todo{
-			Id:     1,
+			Id:     uint16(len(todos)) + 1,
 			Name:   *name,
 			Desc:   *desc,
 			Status: "uncomplete",
 		}
-		json, _ := json.Marshal(todo)
-		os.WriteFile(path, json, 0777)
-	case "get":
-		var todos []Todo
-		content, _ := os.ReadFile(path)
-		json.Unmarshal(content, &todos)
 
+		todos = append(todos, todo)
+		json, _ := json.Marshal(todos)
+		os.WriteFile(path, json, 0777)
+
+	// get subcommand function
+	case "get":
+		get.Parse(flag.Args()[1:])
+
+		idStr := get.Arg(0)
+		if idStr == "" {
+			fmt.Println(todos)
+			return
+		}
+
+		id, _ := strconv.ParseUint(idStr, 10, 16)
+
+		for _, v := range todos {
+			if v.Id == uint16(id) {
+				fmt.Println(v)
+			}
+		}
 	}
 }
